47cff71f73bfa15d404032b9a5c74bada247d028
Farthing <huquanquan22@mails.ucas.ac.cn>
2023-02-17 09:09:20 +0000

    First commit

---
+++ GTLS/core.py
@@ -0,0 +1,262 @@
+import numpy
+import numpy as np
+import numba
+from transitleastsquares.grid import T14
+from transitleastsquares.helpers import running_mean
+import transitleastsquares.tls_constants as tls_constants
+import time
+import multiprocessing
+import cupy as cp
+import cupyFun
+from tqdm import tqdm
+
+def calcGridBlockSize(size):
+    MAX_BLOCK_SIZE = 128
+    # MAX_BLOCK_SIZE = 256
+    # MAX_BLOCK_SIZE = 1024
+    blockSize = size
+    if blockSize > MAX_BLOCK_SIZE:
+        blockSize = MAX_BLOCK_SIZE
+    gridSizeX = int((size / blockSize) + 1)
+    return blockSize,gridSizeX
+
+def runningMeanBulkGPU2(data, width_signals):
+    # width_signals Should be sorted
+    """Returns the running mean in a given window"""
+    cumsums = cp.cumsum(data, axis=1)
+    meanSize = data.shape[1]-width_signals[0] + 1
+    mean = cp.zeros((data.shape[0],width_signals.shape[0], meanSize.item()))
+    for data_index,cumsum in enumerate(cumsums):
+        for index,width_signal in enumerate(width_signals):
+            if(index == 0):
+                mean[data_index][index][1:] = (cumsum[width_signal:] - cumsum[:-width_signal]) / float(width_signal)
+            else:
+                mean[data_index][index][1:-((width_signal) - width_signals[0])] = (cumsum[width_signal:] - cumsum[:-width_signal]) / float(width_signal)
+            mean[data_index][index][0] = data[data_index][:width_signal].mean()
+    return mean
+
+def runningMeanBulkGPU3(data, width_signals):
+    # width_signals Should be sorted
+    """Returns the running mean in a given window"""
+    cumsums = cp.cumsum(data, axis=1)
+    meanSize = data.shape[1]-width_signals[0] 
+    mean = cp.zeros((width_signals.shape[0],data.shape[0], meanSize.item()))
+
+    for index,width_signal in enumerate(width_signals):
+        if(index == 0):
+            mean[index] = (cumsums[:, width_signal:] - cumsums[:, :-width_signal]) / float(width_signal)
+        else:
+            mean[index,:,:-(width_signal-width_signals[0])] = (cumsums[:, width_signal:] - cumsums[:, :-width_signal]) / float(width_signal)
+    return mean
+
+def search_multi_periods(
+    periods,
+    t,
+    y,
+    dy,
+    transit_depth_min,
+    R_star_min,
+    R_star_max,
+    M_star_min,
+    M_star_max,
+    lc_arr,
+    lc_cache_overview,
+    T0_fit_margin,
+    show_progress_bar
+):
+    #PreProcess
+    singleCalcPeriods = 130
+    start = time.time()
+    print('Running PreProcess')
+    with open ('cupyFun.cu', 'r') as myfile:
+        myCode=myfile.read()
+
+    module = cp.RawModule(code=myCode)
+
+    periods = np.sort(periods)
+    periods_arr = np.array(periods).astype(numpy.float32)
+    periodsGPU = cp.asarray(periods_arr)
+
+    durations = numpy.unique(lc_cache_overview["width_in_samples"])
+    maxWidthInSamples = int(max(durations))
+    if maxWidthInSamples % 2 != 0:
+        maxWidthInSamples = maxWidthInSamples + 1
+    
+    durations = numpy.sort(durations)
+
+    # Phase fold
+    mempool = cp.get_default_memory_pool()
+    phasesGPU = cp.empty((len(periods),len(t)),dtype=cp.float32)
+    sortIndexGPU = cp.empty((len(periods),len(t)),dtype=cp.int32)
+    tGPU = cp.asarray(t).astype(cp.float32)
+    periodsSizeGPU = cp.asarray(np.array([len(periods)])).astype(cp.int32)
+    tSizeGPU = cp.asarray(np.array([len(t)])).astype(cp.int32)
+
+    fastFoldGPU = module.get_function('foldFast')
+    blockSize,gridSizeX = calcGridBlockSize(len(t))
+    fastFoldGPU((gridSizeX,len(periods),),(blockSize,), (tGPU, periodsGPU,phasesGPU,periodsSizeGPU,tSizeGPU))
+
+    #To control the GPU memory usage, sort the data in serveral parts
+    i_max = 10
+    for i in range(1,i_max + 1):
+        sortIndexGPU[(i-1)*len(periods)/i_max:i*len(periods)/i_max] = phasesGPU[(i-1)*len(periods)/i_max:i*len(periods)/i_max].argsort()
+    
+    del phasesGPU
+
+    patchedDatasGPU = cp.empty((len(periods),len(t) + maxWidthInSamples),dtype=cp.float32)
+    patchedDysGPU = cp.empty((len(periods),len(t) + maxWidthInSamples),dtype=cp.float32)
+    patchedDatasSizeGPU = cp.asarray(np.array([len(t) + maxWidthInSamples])).astype(cp.int32)
+    patchedDatasSize = int(len(t) + maxWidthInSamples)
+    yGPU = cp.asarray(y).astype(cp.float32)
+    dyGPU = cp.asarray(dy).astype(cp.float32)
+
+    for i in range(len(periods)):
+        patchedDatasGPU[i] = cp.append(yGPU[sortIndexGPU[i]],yGPU[sortIndexGPU[i]][:maxWidthInSamples])
+        patchedDysGPU[i] = cp.append(dyGPU[sortIndexGPU[i]],dyGPU[sortIndexGPU[i]][:maxWidthInSamples])
+
+    del yGPU, dyGPU, sortIndexGPU
+
+    #Other GPU variables, decalre here to save time.
+    inverseSquaredPatchedDysGPU = cp.empty((len(periods),len(t) + maxWidthInSamples),dtype=cp.float32)
+    edgeEffectCorrectionsGPU = cp.empty((len(periods)),dtype=cp.float32)
+    maxwidthInSamplesGPU = cp.asarray(np.array([maxWidthInSamples])).astype(cp.int32)
+    periodSizeGPU = cp.asarray(np.array([len(periods)])).astype(cp.int32)
+    singleCalcPeriodsGPU = cp.asarray(np.array([singleCalcPeriods])).astype(cp.int32)
+    cumsumGPU = cp.empty((singleCalcPeriods,patchedDatasSize),dtype=cp.float32)
+    meanXSizeGPU = cp.asarray(np.array([int(patchedDatasSize) - (np.min(durations)) + 1])).astype(cp.int32)
+    durationsGPU = cp.asarray(durations).astype(cp.int32)
+    durationsSizeGPU = cp.asarray(np.array([len(durations)])).astype(cp.int32)
+    iterFlagGPU = cp.asarray(np.array([0])).astype(cp.int32)
+    ootrGPU = cp.empty((singleCalcPeriods,len(durations),(int(patchedDatasSize) - (np.min(durations)) + 1)),dtype=cp.float32)
+    lowestResidualsGPU = cp.empty((singleCalcPeriods,len(durations),(int(patchedDatasSize) - (np.min(durations)) + 1)),dtype=cp.float32)
+    depthsGPU = cp.empty((singleCalcPeriods,len(durations),(int(patchedDatasSize) - (np.min(durations)) + 1)),dtype=cp.float32)
+    overshootGPU = cp.array(lc_cache_overview["overshoot"]).astype(cp.float32)
+    datapointsGPU = cp.array([len(y)]).astype(cp.int32)
+    meanSizeGPU = cp.array([(patchedDatasSize - x + 1) for x in durations]).astype(np.int32)
+    LowestResidualsEachPeriodGPU = cp.empty((len(periods)),dtype=cp.float32)
+
+    ResSize = int(np.ceil(len(periods) / singleCalcPeriods))
+    tempResultIndexsGPU = cp.empty((ResSize),dtype=np.int)
+    tempResultResidualsGPU = cp.empty((ResSize),dtype=np.float32)
+    tempResultDepthsGPU = cp.empty((ResSize),dtype=cp.float32)
+    # print('ootrSize',ootrGPU.nbytes/1024 **3,'GB')
+    print('memPool',mempool.used_bytes()/1024 **3,'GB')
+    # while True:
+    #     time.sleep(1)
+    print('preProcessTime',time.time()-start)
+
+    # # ##calculate durations
+    # # Not apply the duration filter for now
+    # duration_max = T14(R_s=R_star_max, M_s=M_star_max, P=periods[-1], small=False)
+    # duration_min = T14(R_s=R_star_min, M_s=M_star_min, P=periods[-1], small=True)
+    # ## Fractional transit duration can be longer than this.
+    # ## Example: Data length 11 days, 2 transits at 0.5 days and 10.5 days
+    # length = max(t) - min(t)
+    # no_of_transits_naive = length / periods[-1]
+    # no_of_transits_worst = no_of_transits_naive + 1
+    # correction_factor = no_of_transits_worst / no_of_transits_naive
+    # duration_min_in_samples = int(numpy.floor(duration_min * len(y)))
+    # duration_max_in_samples = int(numpy.ceil(duration_max * len(y) * correction_factor))
+    # durations = durations[durations >= duration_min_in_samples]
+    # durations = durations[durations <= duration_max_in_samples]
+
+
+    remain_index = []
+    for duration in durations:
+        remain_index.append(np.where(lc_cache_overview["width_in_samples"] == duration)[0][0])
+    raw_lc_cache_overview = lc_cache_overview
+    raw_lc_arr = lc_arr
+    lc_arr = lc_arr[remain_index]
+    lc_cache_overview = lc_cache_overview[remain_index]
+    overshoot = np.array(lc_cache_overview["overshoot"]).astype(np.float32)
+
+    lc_arr_len = np.array([len(x) for x in lc_arr]).astype(np.int32)
+    lc_arr_max_len = np.array([np.max(lc_arr_len)]).astype(np.int32)
+    lc_arr_full_length = np.array([np.pad(x, (0, lc_arr_max_len[0] - len(x)), 'constant') for x in lc_arr])
+    lcArrLenGPU = cp.asarray(lc_arr_len).astype(cp.int32)
+    lcArrMaxLenGPU = cp.asarray(lc_arr_max_len).astype(cp.int32)
+    lcArrFullLengthGPU = cp.asarray(lc_arr_full_length).astype(cp.float32)
+
+    calcInverseSquaredPatchedDyGPU = module.get_function('calcInverseSquaredPatchedDy')
+    blockSize,gridSizeX = calcGridBlockSize(patchedDatasSize)
+    calcInverseSquaredPatchedDyGPU((gridSizeX,len(periods),1),(blockSize,1,1),
+    (inverseSquaredPatchedDysGPU,patchedDysGPU,patchedDatasSizeGPU,))
+
+    calcEdgeEffectCorrectionsGPU = module.get_function('calcEdgeEffectCorrections')
+    blockSize,gridSizeX = calcGridBlockSize(len(periods))
+    calcEdgeEffectCorrectionsGPU((gridSizeX,1,1),(blockSize,1,1),
+    (edgeEffectCorrectionsGPU,patchedDatasGPU,inverseSquaredPatchedDysGPU,
+    patchedDatasSizeGPU,maxwidthInSamplesGPU,periodSizeGPU,))
+
+    #From now on, due to memory limitation, GPU can only do several periods(about 100-1000) at a time.
+    # start_calc = time.time()
+    min_locations = []
+    min_residuals = []
+    TotalIterNum = int(np.ceil(len(periods) / singleCalcPeriods))
+    
+    if(show_progress_bar):
+        bar_format = "{desc}{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} periods | {elapsed}<{remaining}"
+        pbar = tqdm(total=TotalIterNum, smoothing=0.3, bar_format=bar_format)
+
+    for iterFlag in range(TotalIterNum):
+    # for iterFlag in range(1):
+   
+        for i in range(singleCalcPeriods):
+            if((iterFlag * singleCalcPeriods + i) < len(periods)):
+                cumsumGPU[i] = cp.cumsum(patchedDatasGPU[i + iterFlag * singleCalcPeriods])
+
+        calcAllOutOfTransitResiduals_step1_2GPU = module.get_function('calcAllOutOfTransitResiduals_step1_2GPU')
+        blockSize,gridSizeX = calcGridBlockSize(patchedDatasSize - (np.min(durations)) + 1)
+        calcAllOutOfTransitResiduals_step1_2GPU((gridSizeX,len(durations),singleCalcPeriods),
+        (blockSize,1,1),(ootrGPU,patchedDatasGPU,durationsGPU,durationsSizeGPU,
+        inverseSquaredPatchedDysGPU,patchedDatasSizeGPU,meanXSizeGPU,
+        iterFlagGPU,singleCalcPeriodsGPU,periodSizeGPU,))
+
+        ootrGPU = np.cumsum(ootrGPU,axis=-1)
+        calcAllOutOfTransitResiduals_step2_2GPU = module.get_function('calcAllOutOfTransitResiduals_step2_2GPU')
+        blockSize,gridSizeX = calcGridBlockSize(len(durations))
+        calcAllOutOfTransitResiduals_step2_2GPU((gridSizeX,singleCalcPeriods,1),
+        (blockSize,1,1),(ootrGPU,patchedDatasGPU,
+        inverseSquaredPatchedDysGPU,durationsGPU,durationsSizeGPU,
+        patchedDatasSizeGPU,meanXSizeGPU,
+        iterFlagGPU,singleCalcPeriodsGPU,periodSizeGPU,))
+
+        calcAllLowestResidualsGPU = module.get_function('calcAllLowestResidualsGPU')
+        blockSize,gridSizeX = calcGridBlockSize(patchedDatasSize - (np.min(durations)) + 1)
+        
+        calcAllLowestResidualsGPU((gridSizeX,len(durations),singleCalcPeriods),
+        (blockSize,1,1),(lowestResidualsGPU,depthsGPU,meanSizeGPU,
+        meanXSizeGPU,patchedDatasGPU,patchedDatasSizeGPU,durationsGPU,
+        durationsSizeGPU,lcArrFullLengthGPU,lcArrLenGPU,lcArrMaxLenGPU,inverseSquaredPatchedDysGPU,
+        overshootGPU,ootrGPU,edgeEffectCorrectionsGPU,datapointsGPU,cumsumGPU,#meanGPU,
+        iterFlagGPU,singleCalcPeriodsGPU,periodSizeGPU,))
+
+        locationGPU = lowestResidualsGPU.argmin()
+        minValue = lowestResidualsGPU.min()
+        tempResultIndexsGPU[iterFlag] = locationGPU.item()
+        tempResultResidualsGPU[iterFlag] = minValue
+        sub_location = cp.unravel_index(locationGPU, lowestResidualsGPU.shape)
+        tempResultDepthsGPU[iterFlag] = depthsGPU[sub_location[0]][sub_location[1]][sub_location[2]]
+
+        if((iterFlag+1)*singleCalcPeriods < len(periods)):
+            LowestResidualsEachPeriodGPU[(iterFlag)*singleCalcPeriods:(iterFlag+1)*singleCalcPeriods] = lowestResidualsGPU.min(axis=(1,2))
+        else:
+            LowestResidualsEachPeriodGPU[(iterFlag)*singleCalcPeriods:] = lowestResidualsGPU[:len(periods) - (iterFlag)*singleCalcPeriods].min(axis=(1,2))
+
+        iterFlagGPU = iterFlagGPU + 1
+        if show_progress_bar:
+            pbar.update(1)
+
+    if show_progress_bar:
+        pbar.close()
+    # minRes = tempResultResidualsGPU.min()
+    # print('minRes',minRes)
+    
+    location = np.argmin(tempResultResidualsGPU.get())
+    subLocation = cp.unravel_index(tempResultIndexsGPU[location], lowestResidualsGPU.shape)
+    bestPeriod = periods[location * singleCalcPeriods + subLocation[0].item()]
+    rawDuration = durations[subLocation[1].item()]
+    bestT0 = (bestPeriod/len(t))*(subLocation[2].item()+rawDuration)
+    bestDuration = (rawDuration*bestPeriod) / len(t)
+    bestDepth = 1 - tempResultDepthsGPU[location].item()
+    return LowestResidualsEachPeriodGPU.get(),bestPeriod,bestDuration,bestDepth,bestT0
---
+++ GTLS/main.py
@@ -0,0 +1,81 @@
+# The GTLS(GPU Transit Least Squares) algorithm is adapted from the TLS(Transit Least Squares) algorithm by Michael Hippke & René Heller (2019)
+# The TLS is an open source software with MIT license. The copyright of the TLS algorithm is held by the authors.
+# You can find the original paper here: https://ui.adsabs.harvard.edu/abs/2019A%26A...623A..39H/abstract
+
+# The GTLS algorithm is also an open source software with MIT license.
+
+import numpy as np
+import cupy as cp
+from validate import validate_inputs,validateAndChooseDevice,validate_args
+import core as core
+import tls_constants as tls_constants
+from grid import duration_grid, period_grid
+from transit import reference_transit, fractional_transit, get_cache
+from stats import spectra
+
+class gtls(object):
+    """Compute the transit least squares of limb-darkened transit models using GPU"""
+
+    def __init__(self,t,y,dy = None,verbose = False):
+        self.t, self.y, self.dy = validate_inputs(t, y, dy)
+        self.verbose = verbose
+    
+    def power(self,**kwargs):
+        self, kwargs = validate_args(self, kwargs)
+        # self.show_progress_bar = True
+        self.device = validateAndChooseDevice(self.deviceId)
+        if self.device == None:
+            return
+        # if self.verbose:
+        #     print(tls_constants.TLS_VERSION)
+
+        # Generate possible periods
+        periods = period_grid(
+            R_star=self.R_star,
+            M_star=self.M_star,
+            time_span=np.max(self.t) - np.min(self.t),
+            period_min=self.period_min,
+            period_max=self.period_max,
+            oversampling_factor=self.oversampling_factor,
+            n_transits_min=self.n_transits_min,
+        )
+
+        # Generate possible durations
+        durations = duration_grid(
+            periods, shortest=1 / len(self.t), log_step=self.duration_grid_step
+        )
+        # maxwidth_in_samples : durations width in the longest transit
+        maxwidth_in_samples = int(np.max(durations) * np.size(self.y))
+        if maxwidth_in_samples % 2 != 0:
+            maxwidth_in_samples = maxwidth_in_samples + 1
+        lc_cache_overview, lc_arr = get_cache(
+            durations=durations,
+            maxwidth_in_samples=maxwidth_in_samples,
+            per=self.per,
+            rp=self.rp,
+            a=self.a,
+            inc=self.inc,
+            ecc=self.ecc,
+            w=self.w,
+            u=self.u,
+            limb_dark=self.limb_dark,
+            verbose=self.verbose
+        )
+
+        chi2,period,duration,depth,T0 = core.search_multi_periods(
+            periods=periods,
+            t=self.t,
+            y=self.y,
+            dy=self.dy,
+            transit_depth_min=self.transit_depth_min,
+            R_star_min=self.R_star_min,
+            R_star_max=self.R_star_max,
+            M_star_min=self.M_star_min,
+            M_star_max=self.M_star_max,
+            lc_arr=lc_arr,
+            lc_cache_overview=lc_cache_overview,
+            T0_fit_margin=self.T0_fit_margin,
+            show_progress_bar=self.show_progress_bar,
+        )
+        SR, power_raw, power, SDE_raw, SDE = spectra(chi2, self.oversampling_factor)
+        return period, duration, depth, T0, SDE
